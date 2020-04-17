6d9f472460292394c87b2be6b3de03339b3e1b26
Gr3yG00se64 <Gr3yG00se@protonmail.com>
2020-04-17 15:51:31 -0400

    Changed outfile write mode

--- setup/add_alerts.py
+++ setup/add_alerts.py
@@ -1,6 +1,7 @@
 import json
 
 data = []
+
 data.append({
     'ts': '1574053161.804296',
     'uid': 'C7Nzqy4J1H3f9JUDed',
@@ -24,14 +25,15 @@ data.append({
     'id.orig_p': '51508',
     'id.resp_h': '23.79.244.155',
     'id.resp_p': '80',
-    'trans_depth': '2,',
+    'trans_depth': '2',
     'method': 'GET',
     'host': 'malware.testing.google.test/testing/malware/',
     'uri': '/filestreamingservice/files/fc989f39-6b38-4ee7-b13a-6f6b68435329?P1=1574053617&P2=402&P3=2&P4=VwbJChfabzKiBTXbHqdbcrCbu7+SUgA15enBstQtHr1qZRdvZo9dYhm52BIPOpPz5uFEbENvTEv/G0awych5Sw==',
     'user_agent': 'Microsoft-Delivery-Optimization/10.0',
-    'request_body_len': '0,',
+    'request_body_len': '0',
     'tags': []
 })
-#with open('/Users/joshuageise/Projects/overwatch/setup/http.log', 'a+') as outfile:
-with open('/usr/local/zeek/logs/current/http.log', 'a+') as outfile:
+
+#with open('/Users/joshuageise/Projects/overwatch/setup/http.log', 'w+') as outfile:
+with open('/usr/local/zeek/logs/current/http.log', 'w+') as outfile:
     json.dump(data, outfile)
