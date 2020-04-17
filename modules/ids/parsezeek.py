5dc6ed375f58483686efaae95fc5ba6249532f1b
Gr3yG00se64 <Gr3yG00se@protonmail.com>
2020-04-17 13:48:00 -0400

    Fixed ParseZeek

--- modules/ids/parsezeek.py
+++ modules/ids/parsezeek.py
@@ -2,33 +2,26 @@
 
 #Local Dependencies
 import config
+
 #Remote Dependencies
 import os
 import json
 
 def http_parse():
-    #Prep Parser Value Location Variables
-    ts = 0
-    origIP = 2
-    origPort = 3
-    respIP = 4
-    respPort = 5
-    host = 8
 
-    #Create list for
     http_results = []
 
-    if (os.path.exists(config.zeek_log_dir+'http.log')):
-        with open(config.zeek_log_dir+'http.log') as host_file:
-            for line in host_file:
-                base_split = line.split('\'')[0].split(',')
+    if (os.path.exists(config.zeek_log_dir + 'http.log')):
+        with open(config.zeek_log_dir + 'http.log', 'r') as f:
+            http_log = json.load(f)
 
-                http_results.append({'ts': base_split[ts].split(':')[1],
-                                'origIP': base_split[origIP].split(':')[1],
-                                'origPort': base_split[origPort].split(':')[1],
-                                'respIP': base_split[respIP].split(':')[1],
-                                'respPort': base_split[respPort].split(':')[1],
-                                'host': base_split[host].split(':')[1]
-                                })
+            for result in http_log:
+                http_results.append({'ts': result['ts'],
+                                     'origIP': result['id.orig_h'],
+                                     'origPort': result['id.orig_p'],
+                                     'respIP': result['id.resp_h'],
+                                     'respPort': result['id.resp_p'],
+                                     'host': result['host']
+                                     })
 
     return http_results
